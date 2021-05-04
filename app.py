02b63dfb25d48652edb4f9d3785e2e2b2173f540
BCHR <bchr@novozymes.com>
2021-05-04 13:06:19 +0200

    letter

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ app = Flask('__name__')
 
 
 @app.route("/")
-def index():
-    greeting = "Congratulations, Radu, you'll be sorely missed"
-    return greeting
+def index():    
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
